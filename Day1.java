1becc62e9a3f2b07de3ad5cdd57bf8ef5bbdd388
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-02 23:13:07 +0800

    Create Day1.java with comments and main method
    Add comments and a main method to Day1 class

---
+++ Day1.java
@@ -0,0 +1,21 @@
+/**       tanda di samping merupakan simbol dari javadoc yang artinya catatan atau penjelasn
+ * Day1   khusus pada kode java yang bisa digunakan untuk membuat dokumentasi
+ */
+public class Day1 {
+
+    public static void main(String[] args) {
+        System.out.println("hallow word");
+
+// tanda di samping merupakan simbol dari single line yang artinya hanya bisa satu komentar saja
+
+/*dan tanda di samping
+merupakan simbol
+dari multi line yang artinya 
+bisa menggunakan lebih dari satu komentar
+*/
+
+
+
+
+
+    }
