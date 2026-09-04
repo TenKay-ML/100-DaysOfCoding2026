175bb4a29e9bdc934e894f6bbc3236a3b48d179c
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-04 19:00:06 +0800

    Add Day3.java with personal introduction

---
+++ Day3.java
@@ -0,0 +1,15 @@
+/**
+ * App
+ */
+public class App {
+public static void main(String[] args) {
+    
+    System.out.print("===\r=====Sedikit Tentang Saya=====\n");
+    System.out.println("nn\bama saya Alhadi Nurrahman M");
+    System.out.println("hobi saya adalah \"BERENANG\"\f");
+    System.out.println("saya berasal dari ;");
+    System.out.print("kecamatan \t: polewali mandar\nprovinsi \t: sulawesi barat\n");
+    System.out.println("\\===========================\\");
+}
+    
+}
