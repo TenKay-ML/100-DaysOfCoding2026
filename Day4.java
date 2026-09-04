8f605029532c4a79f69d6de6039a4d66a03480a5
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-05 01:33:29 +0800

    Add Day4.java for sales data display

---
+++ Day4.java
@@ -0,0 +1,20 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        
+        byte jumlahbarang = 43;
+        short stok = 1000;
+        int harga = 25500;
+        long total = 1096500L;
+
+        System.out.println("==========DATA PENJUALAN==========");
+        System.out.println("barang yang terjual \t: " + jumlahbarang);
+        System.out.println("stok barang \t\t: " + stok);
+        System.out.println("harga satuan \t\t: Rp" + harga);
+        System.out.println("total uang masuk \t: Rp" + total);
+        System.out.println("==================================");
+    }
+}
