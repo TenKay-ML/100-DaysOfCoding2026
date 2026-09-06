56b0d829f4816a2099637602dfab9972d4d6da43
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-06 10:22:12 +0800

    Add Java program to demonstrate double and float types
    This Java program demonstrates the use of double and float data types, displaying their values and min/max constants.

---
+++ Day5.java
@@ -0,0 +1,34 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+    public static void main(String[] args) {
+        double tinggibadan = 163.5;
+        float beratbadan = 48.6f;
+
+        System.out.println("========== TIPE DATA DOUBLE ========== ");
+        System.out.println("nilai tinggi badan \t: " + tinggibadan + " cm");
+        System.out.println("Double MIN_VALUE \t: " + Double.MIN_VALUE);
+        System.out.println("Double MAX_VALUE \t: " + Double.MAX_VALUE);
+        System.out.println("======================================");
+
+        System.out.println("\n");
+
+        System.out.println("========== TIPE DATA FLOAT =========== ");
+        System.out.println("nilai berat badan \t: " + beratbadan + " kg");
+        System.out.println("Float MIN_VALUE \t: " + Float.MIN_VALUE);
+        System.out.println("Float MAX_VALUE \t: " + Float.MAX_VALUE);
+        System.out.println("======================================");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+       
