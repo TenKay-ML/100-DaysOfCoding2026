a2ee67aa1d06a90506bb268d07fa498818ee21de
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-07 19:09:00 +0800

    Add Day6.java with basic Java data types example

---
+++ Day6.java
@@ -0,0 +1,28 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+    public static void main(String[] args) {
+        
+        // char adalah tipe data yang menyimpan sebuah karakter seperti huruf, angka, dan simbol
+
+        char jenisKelamin = 'L';
+        char nilai = 'A';
+
+        System.out.println("JENIS KELAMIN \t:" + jenisKelamin);
+        System.out.println("Nilai ku di semester ini adalah " + nilai );
+
+        System.out.println("\n");
+
+        // boolean adalah tipe data yang menyimpan nilai benar atau salah
+
+        boolean hidup = true;
+        boolean lulus = false;
+
+        System.out.println("Apakah aku masih hidup : " + hidup);
+        System.out.println("Apakah aku lulus di semester ini : " + lulus);
+    
+    }
+
+    
+}
