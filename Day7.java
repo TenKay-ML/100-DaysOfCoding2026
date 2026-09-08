121abbc911ca3ed820253d219e48598c5a8dcf02
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-08 19:24:35 +0800

    Add Day7.java with basic Java program

---
+++ Day7.java
@@ -0,0 +1,20 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+
+        //Tipe data string adalah tipe data yang digunakan untuk menyimpan teks atau kumpulan karakter
+        String nama = "Alhadi Nurrahman Majid";
+        String namaPanggilan ="Aldi";
+        String suku = "JAWA";
+        String status = "MAHASISWA";
+
+        System.out.println("NAMA \t\t\t: " + nama);
+        System.out.println("NAMA PANGGILAN \t\t: " + namaPanggilan);
+        System.out.println("SUKU \t\t\t: " + suku);
+        System.out.println("STATUS \t\t\t: " +status);
+
+    }
+}
