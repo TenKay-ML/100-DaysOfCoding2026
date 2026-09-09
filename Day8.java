b1de0c72807184166fcee2a81c543fa59b201ebe
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-09 19:46:06 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,20 @@
+public class belajarjava {
+    public static void main(String[] args) {
+        
+        //Deklarasi adalah proses membuat atau menentukan sebuah variabel dengan menyebutkan tipe datanya. contohnya seperti :
+        String hari;
+        int tanggal;
+
+        //Inisialisasi adalah proses memberikan nilai awal kepada variabel yang sudah dideklarasi. contohnya seperti :
+        hari = "SENIN";
+        tanggal = 13;
+
+        System.out.println("sekarang hari " + hari + " bertepatan pada tanggal " + tanggal );
+
+        //Update adalah proses mengubah nilai variabel yang sebelumnya sudah memiliki nilai. contohnya seperti :
+        hari = "SELASA";
+        tanggal = 14;
+
+        System.out.println("sekarang hari " + hari + " bertepatan pada tanggal " + tanggal );
+    }
+}
