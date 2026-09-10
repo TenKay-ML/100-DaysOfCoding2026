2fc8db3f171c116cef83fd041541ccd60fb71925
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-10 09:50:04 +0800

    Add Day9.java with main method and constant example

---
+++ Day9.java
@@ -0,0 +1,13 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        
+        //Kostanta adalah nilai yang sudah tetap, tetap artinya nilai yang sudah final atau tidak bisa di update. contohnya
+        final int JUMLAH_JAM = 24;
+
+        System.out.println("dalam satu hari itu memiliki " + JUMLAH_JAM + " jam");
+    }
+}
