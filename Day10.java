96eb9b9cdc4a16bc1219537c632fe7321fd12647
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-11 19:22:06 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,43 @@
+import java.nio.channels.Pipe.SourceChannel;
+
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        
+        //Batas minimal dan maksimal tipe data adalah nilai terkecil dan nilai terbesar yang dapat disimpan oleh suatu tipe data dalam program.
+       System.out.println("=================================================");
+        System.out.println("TIPE DATA BYTE");
+        System.out.println("batas minimalnya adalah = " + Byte.MIN_VALUE);
+        System.out.println("batas maksimalnya adalah = "+ Byte.MAX_VALUE);
+        System.out.println("\n");
+
+        System.out.println("TIPE DATA SHORT");
+        System.out.println("batas minimalnya adalah = " + Short.MIN_VALUE);
+        System.out.println("batas maksimalnya adalah = " + Short.MAX_VALUE);
+        System.out.println("\n");
+
+        System.out.println("TIPE DATA INTEGER");
+        System.out.println("batas minimalnya adalah = " + Integer.MIN_VALUE);
+        System.out.println("batas maksimalnya adalah = " + Integer.MAX_VALUE);
+        System.out.println("\n");
+
+        System.out.println("TIPE DATA LONG");
+        System.out.println("batas minimalnya adalah = " + Long.MIN_VALUE);
+        System.out.println("batas maksimalnya adalah = " + Long.MAX_VALUE);
+        System.out.println("\n");
+
+        System.out.println("TIPE DATA FLOAT");
+        System.out.println("batas minimalnya adalah = " + Float.MAX_VALUE);
+        System.out.println("batas maksimalnya adalah = " + Float.MAX_VALUE);
+        System.out.println("\n");
+
+        System.out.println("TIPE DATA DOUBLE");
+        System.out.println("batas minimalnya adalah = " + Double.MIN_VALUE);
+        System.out.println("batas maksimalnya adalah = " + Double.MAX_VALUE);
+        System.out.println("==================================================");
+
+    }
+}
