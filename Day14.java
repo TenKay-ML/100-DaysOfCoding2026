485bdf0eed6139b9f03f0349fb075d1287cf9d23
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-15 17:59:29 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        int ang1 = 13;
+        int ang2 = 4;
+        int hasil1 = ang1 + ang2;
+        int hasil2 = ang1 - ang2;
+
+        System.out.println("Hasil Dari Penjumblahan (13 + 4) Adalah : "+ hasil1);
+         System.out.println("Hasil Dari Pengurangan (13 - 4) Adalah : "+ hasil2);
+    }
+}
