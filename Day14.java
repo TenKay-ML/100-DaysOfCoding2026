d94afb6d0f08a962596f0d378f08d7f929a2fc3a
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-15 19:18:59 +0800

    Update Day14.java

--- Day14.java
+++ Day14.java
@@ -4,10 +4,10 @@
 public class belajarjava {
 
     public static void main(String[] args) {
-        int ang1 = 13;
-        int ang2 = 4;
-        int hasil1 = ang1 + ang2;
-        int hasil2 = ang1 - ang2;
+        int A = 13;
+        int B = 4;
+        int hasil1 = A + B;
+        int hasil2 = A - B;
 
         System.out.println("Hasil Dari Penjumblahan (13 + 4) Adalah : "+ hasil1);
          System.out.println("Hasil Dari Pengurangan (13 - 4) Adalah : "+ hasil2);
