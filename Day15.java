ed0e72c843b15ccfb003832d0a98f9cc8a90e66f
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-16 17:03:22 +0800

    Add basic multiplication and division in Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        int A = 20;
+        int B = 4;
+        int hasil1 = A * B;
+        int hasil2 = A / B;
+
+        System.out.println("Hasil Dari Perkalian (20 x 4) Adalah : "+ hasil1);
+         System.out.println("Hasil Dari Pembagian (20 / 4) Adalah : "+ hasil2);
+    }
+}
