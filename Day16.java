397212161d50f43c3322cec6c3615f124f4137b2
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-17 19:18:05 +0800

    Add Day16.java for modulus operation example

---
+++ Day16.java
@@ -0,0 +1,16 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        int A = 45;
+        int B = 6;
+
+        int hasil1 = A % B;
+        int hasil2 = B % A;
+        
+        System.out.println("Hasil Dari Sisa Pembagian (45 % 6) Adalah : "+ hasil1);
+        System.out.println("Hasil Dari Sisa Pembagian (6 % 45) Adalah : "+ hasil2);
+    }
+}
