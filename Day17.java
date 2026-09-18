b11a20f384b9d15af9416479c6c8df8eae10561f
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-18 19:23:25 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,30 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        
+        int A = 30;
+        System.out.println("nilai awal variabel A adalah "+ A);
+
+        A += 5;
+        System.out.println("nilai variabel A setelah ditambah adalah "+ A);
+
+        A -= 10;
+        System.out.println("nilai variabel A setelah dikurang adalah "+ A);
+
+        A *= 4;
+        System.out.println("nilai variabel A setelah dikali adalah "+ A);
+
+        A /= 10;
+        System.out.println("nilai varibel A setelah dibagi adalah "+ A);
+
+        A %= 10;
+        System.out.println("sisa bagi hasil variabel A adalah "+ A);
+        
+    }
+}
+
+
+   
