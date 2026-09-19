7d41c90ae68fd49e61498e0a4b54f74ef20354e6
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-19 09:45:20 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,24 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        
+        byte A = 15;
+        short B = A;
+        int C = B;
+        long D = C;
+        float E = D;
+        double F = E;
+
+        System.out.println("nilai awal byte \t\t\t= "+ A);
+        System.out.println("byte setelah di konversi ke short \t= "+ B);
+        System.out.println("short setelah di konversi ke integer \t= "+ C);
+        System.out.println("integer setelah di konversi ke long \t= "+ D);
+        System.out.println("long setelah di konversi ke float \t= "+ E);
+        System.out.println("float setelah di konversi ke double \t= "+ F);
+
+        
+    }
+}
