2ccd644fb7243906535ded15314c783a9a309ec0
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-20 19:44:55 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+public class javayey {
+
+    public static void main(String[] args) {
+        long a = 9876543212345L;
+        int b = (int) a;
+        short c = (short) b;
+        byte d = (byte) c;
+
+        double e = 321.4;
+        float f = (float) e;
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);    
+        System.out.println("Nilai c: " + c);
+        System.out.println("Nilai d: " + d);
+        
+        System.out.println("Nilai e: " + e);
+        System.out.println("Nilai f: " + f);
+    }
+}
