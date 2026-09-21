2ee4183747c013bfe1616217657a88a3d4025ece
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-21 19:23:57 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,18 @@
+public class belajarjava {
+
+public static void main(String[] args) {
+
+int umur = 18;
+double tinggi = 164.4;
+boolean lulus = true;
+
+String umurku = String.valueOf(umur);
+String tinggiku = String.valueOf(tinggi);
+String lulustidak = String.valueOf(lulus);
+
+System.out.println(umurku + " tahun");
+System.out.println(tinggiku + " cm");
+System.out.println("lulus: "+lulustidak);
+
+}
+}
