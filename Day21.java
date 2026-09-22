8f1db40c3662802baadb304018eb20b458c73642
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-22 18:44:53 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,24 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+        String A = "13";
+        String B = "13.2";
+        String C = "false";
+        String E = "@";
+        
+
+        int I = Integer.parseInt(A);
+        double D = Double.parseDouble(B);
+        Boolean L = Boolean.parseBoolean(C);
+        char Z = E.charAt(0);
+
+        System.out.println(I);
+        System.out.println(D);
+        System.out.println(L);
+        System.out.println(Z);
+        
+    }
+}
