9289c804297f27f58b99617dd3145d5a10d5a4ae
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-23 19:11:01 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,26 @@
+/**
+ * belajarjava
+ */
+public class belajarjava {
+
+    public static void main(String[] args) {
+       
+        int a = 5;
+        int b = 10;
+        int c;
+
+        System.out.println("Sebelum ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println("\nSetelah ditukar:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
+        
+    
