6569ea07efc549db6511da4305ac67c3552803c7
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-24 19:00:40 +0800

    Add Java program to calculate square area

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class belajarjava {
+    public static void main(String[] args) {
+       int sisi = 10;
+
+       int luas = sisi * sisi;
+
+       System.out.print("luas persegi yang memiliki sisi masing masing 10cm adalah \n= " + luas + "cm");
+
+    }
+}
