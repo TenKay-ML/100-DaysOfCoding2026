6625c4b4fc4996c58c074cd50a29f6b0850a2516
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-25 21:26:46 +0800

    Add Java program to calculate rectangle area

---
+++ Day24.java
@@ -0,0 +1,10 @@
+public class belajarjava {
+    public static void main(String[] args) {
+      int panjang = 5;
+      int lebar = 7;
+
+  int luas = panjang * lebar;
+
+  System.out.print("luas sebuah persegi panjang yang memiliki panjang 5cm dan lebar 7cm adalah \n = " + luas +" cm");
+    }
+}
