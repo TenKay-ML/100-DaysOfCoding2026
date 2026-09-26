df9cd2374c4e94d9157b9397a73614ff6b460ee5
TenKay-ML <alhadinurrahman08@gmail.com>
2026-09-26 18:23:54 +0800

    Add Java program to calculate circle area

---
+++ Day25.java
@@ -0,0 +1,11 @@
+public class belajarjava {
+    public static void main(String[] args) {
+        int r = 5;
+        double pi = 3.14;
+
+        double luas = pi * r * r;
+
+        System.out.println("Luas lingkaran dengan jari-jari " + r + " cm^2 adalah: " + luas + " cm^2");
+        
+    }
+}
